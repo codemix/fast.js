6e9932ec1acd62dcdc99cc49b37ee7ba70eac584
Charles Pick <charles.pick@gmail.com>
2014-09-28 23:37:16 +0100

    add shim for Object.assign()

---
+++ bench/shims.js
@@ -0,0 +1,45 @@
+'use strict';
+
+if (!Object.assign) {
+  // source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+  Object.defineProperty(Object, "assign", {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function(target, firstSource) {
+      "use strict";
+      if (target === undefined || target === null)
+        throw new TypeError("Cannot convert first argument to object");
+
+      var to = Object(target);
+
+      var hasPendingException = false;
+      var pendingException;
+
+      for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i];
+        if (nextSource === undefined || nextSource === null)
+          continue;
+
+        var keysArray = Object.keys(Object(nextSource));
+        for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+          var nextKey = keysArray[nextIndex];
+          try {
+            var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+            if (desc !== undefined && desc.enumerable)
+              to[nextKey] = nextSource[nextKey];
+          } catch (e) {
+            if (!hasPendingException) {
+              hasPendingException = true;
+              pendingException = e;
+            }
+          }
+        }
+
+        if (hasPendingException)
+          throw pendingException;
+      }
+      return to;
+    }
+  });
+}
