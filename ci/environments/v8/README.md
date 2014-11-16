1b78a8afefc64dad4b0957a14be9dc995a1534f0
Charles Pick <charles.pick@gmail.com>
2014-11-16 01:49:14 +0000

    install v8 shell

---
+++ ci/environments/v8/README.md
@@ -0,0 +1,3 @@
+# V8 Shell
+
+This directory contains the V8 js-shell. Run `npm run install-v8` to install it.
---
+++ ci/environments/v8/install.sh
@@ -0,0 +1,2 @@
+git clone --depth=1 http://github.com/v8/v8-git-mirror
+make -C ./v8-git-mirror builddeps native
